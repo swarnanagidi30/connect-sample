0d8c2246bd163aa48f3b4ea88795c44d777759a6
swarnanagidi30 <swarnangidi30@gmail.com>
2020-08-09 01:46:48 +0530

    read more fix

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
 function changeRead() {
     var element1 = document.getElementById("read-more-click");
-    element1.innerHTML = `Hide <i class="fa fa-chevron-up">`;
+    if (element1.value == 'read') {
+        element1.value = "hide"
+        element1.innerHTML = `Hide <i class="fa fa-chevron-up" data-toggle="collapse" data-target="#boxcollapse">`;
+    } else {
+        element1.value = "read"
+        element1.innerHTML = `Read More <i class="fa fa-chevron-up" data-toggle="collapse" data-target="#boxcollapse">`;
+    }
 }
